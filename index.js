2efe064f08dbadc9dabaf3124a38bda949f988f4
Xiyi Wang <xiyiwang116@outlook.com>
2020-12-14 11:56:08 -0500

    updated index.js to use axios for making restful requests

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 #! /usr/bin/env node
 
+const { default: Axios } = require('axios');
+
 // Create a readline interface
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout,
 })
 
-readline.question(`What would you like to log today? `, item => {
-    console.log(item);
+readline.question(`What would you like to log today? `, async (item) => {
+    const { data } = await Axios.get(`http://localhost:3001/food`);
+    const it = data[Symbol.iterator]();
+    let position = it.next();
+    while (!position.done) {
+        const food = position.value.name;
+        if (food === item ) {
+            console.log(`${item} has ${position.value.calories} calories`);
+        }
+        position = it.next();
+    }
+    // console.log(item);
     readline.close();
 })
