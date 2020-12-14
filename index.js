3a476efaac1fbad1089eefeb3954aacacd0901ac
Xiyi Wang <xiyiwang116@outlook.com>
2020-12-14 11:20:34 -0500

    created a readline interface

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 #! /usr/bin/env node
+
+// Create a readline interface
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+readline.question(`What would you like to log today? `, item => {
+    console.log(item);
+    readline.close();
+})
